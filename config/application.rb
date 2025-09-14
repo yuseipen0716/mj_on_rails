e97792b61ea543f4c4553f6babdc2317d6c6c990
yuseipen0716 <yuseipen1@gmail.com>
2025-09-15 04:20:11 +0900

    add config locale and time_zone

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,12 @@ module App
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
 
+    config.i18n.default_locale = :ja
+
+    # config time_zone
+    config.time_zone = 'Asia/Tokyo'
+
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
